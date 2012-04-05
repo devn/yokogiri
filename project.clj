e1fb622f6a9b379179b547bdb456f581e1062a24
Devin Walters <devinw@gmail.com>
2012-04-04 20:48:50 -0500

    Add URL for project

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject yokogiri "0.0.1"
   :description "Nokogiri for Clojure"
+  :url "https://github.com/devn/yokogiri"
   :dependencies [[org.clojure/clojure "1.4.0-master-SNAPSHOT"]
                  [net.sourceforge.htmlunit/htmlunit "2.9"]
                  [se.fishtank/css-selectors "1.0.2"]]
