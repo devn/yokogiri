efd356aceb98b7799b4e126d51a3c68298fe37f9
Devin Walters <devinw@gmail.com>
2012-04-04 20:44:18 -0500

    Remove extra dependencies

--- project.clj
+++ project.clj
@@ -2,10 +2,5 @@
   :description "Nokogiri for Clojure"
   :dependencies [[org.clojure/clojure "1.4.0-master-SNAPSHOT"]
                  [net.sourceforge.htmlunit/htmlunit "2.9"]
-                 [se.fishtank/css-selectors "1.0.2"]
-                 ;; [nekohtml/nekohtml "1.9.6.2"]
-                 ;; [nekohtml/nekodtd "0.1.11"]
-                 ;; [isorelax/isorelax "20030108"]
-                 ;; [org.apache.camel/camel-jing "2.9.0"]
-                 ]
+                 [se.fishtank/css-selectors "1.0.2"]]
   :dev-dependencies [[midje "1.3.1"]])
