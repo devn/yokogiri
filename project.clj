35b5812b46230c3f3eb345b034585ee445e73d2d
Devin Walters <devinw@gmail.com>
2013-03-28 09:25:39 -0500

    Bump css-selectors version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yokogiri "1.5.1"
+(defproject yokogiri "1.5.2"
   :description "Barebones Nokogiri for Clojure"
   :license {:name "Eclipse Public License - Version 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
@@ -7,6 +7,6 @@
   :url "https://github.com/devn/yokogiri"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [net.sourceforge.htmlunit/htmlunit "2.9"]
-                 [se.fishtank/css-selectors "1.0.2"]]
+                 [se.fishtank/css-selectors "1.0.4"]]
   :profiles {:dev {:dependencies [[midje "1.3.1"]]}}
   :min-lein-version "2.0.0")
