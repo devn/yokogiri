4bfa8ba270fb46a7631297648f560bfb2b6f7aaf
Devin Walters <devinw@gmail.com>
2013-03-28 00:18:16 -0500

    Update deps and bump to 1.5.1

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject yokogiri "1.4.0"
+(defproject yokogiri "1.5.1"
   :description "Barebones Nokogiri for Clojure"
   :license {:name "Eclipse Public License - Version 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :comments "Same as Clojure"
             :distribution :repo}
   :url "https://github.com/devn/yokogiri"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [net.sourceforge.htmlunit/htmlunit "2.9"]
                  [se.fishtank/css-selectors "1.0.2"]]
   :profiles {:dev {:dependencies [[midje "1.3.1"]]}}
